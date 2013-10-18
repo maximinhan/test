6056a98de5ec1631f5c520d074caedf3ad633e5a
maximinhan <maximinhan@gmail.com>
2013-10-19 02:06:24 +0900

    add

---
+++ study02/src/test/java/study02/javacafe/spring02/test/GetDBTest.java
@@ -0,0 +1,14 @@
+package study02.javacafe.spring02.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GetDBTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
