68ac806aef147d8971dca52d47bc94ce3d567635
maximinhan <maximinhan@gmail.com>
2013-10-19 04:52:35 +0900

    주석 달음

--- study02/src/test/java/study02/javacafe/spring02/test/GetDBTest.java
+++ study02/src/test/java/study02/javacafe/spring02/test/GetDBTest.java
@@ -1,14 +1,23 @@
 package study02.javacafe.spring02.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import study.javacafe.spring02.dao.HelloDao;
 
 public class GetDBTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		HelloDao hellodao = context.getBean("helloDao", HelloDao.class);
+
+		assertEquals("Hello Spring!!!", hellodao.get("first").getValue());
 	}
 
 }
